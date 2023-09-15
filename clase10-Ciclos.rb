d753b8695a071ceb0c8315e44cbeea1fc9bed616
leo tatico <leoandrest40@gmail.com>
2023-09-15 12:31:21 -0500

    Ciclos

---
+++ clase10-Ciclos.rb
@@ -0,0 +1,32 @@
+# CICLOS:
+
+
+WHILE:
+
+#x = 1
+#while x < 5 do
+#    puts " holaa #{x}"
+#   x += 1
+#end
+
+LOOP:
+#en este caso este loop es como un ciclo infinito por lo que nosotros tenemos que explicitamente
+#declarar dentro de ese ciclo cuando queremos terminarlo y se hace con la palabra break.
+#x = 1 
+#loop do 
+#    puts "hola #{x}"
+#    x += 1
+#    break if x >= 5
+#end
+
+FOR:
+
+#for i in 1..4 do 
+#    puts "hola #{i}"
+#end
+
+#metodos auxiliares(arrays,rangos)
+
+#[1,2,3,4].each { |x| puts "hola #{x}"}
+
+#4.times { |x| puts "hola #{x}"}
